0c77e039d0a4cc24d10b11b2fbe91d6961a9c07a
sudarshansamula <43838017+sudarshansamula@users.noreply.github.com>
2021-04-01 11:57:00 +0530

    Add files via upload

--- bin/www.js
+++ bin/www.js
@@ -1,1 +1,38 @@
 
+const http = require('http');
+const app = require('../app');
+const db = require('../config/dbController');
+var config = require('../config/index');
+const PORT = config.PORT;
+
+app.set('port', PORT);
+
+const server = (() => http.createServer(app))();
+server.listen(PORT);
+
+async function serverHandler() {
+    console.log(`Server is UP and listening on port ${PORT}`);
+}
+
+async function errorHandler(err) {
+    switch (err.code) {
+    case 'EACCES':
+        console.log('EACCES error');
+        break;
+    case 'EADDRINUSE':
+        console.log('address already in use');
+        break;
+    default:
+        throw err;
+    }
+    process.exit(1);
+}
+
+async function exitHandler() {
+    await db.stopListeners();
+    console.log('Server STOPPED and database connection CLOSED');
+}
+
+server.on('listening', serverHandler);
+server.on('error', errorHandler);
+process.on('exit', exitHandler);
